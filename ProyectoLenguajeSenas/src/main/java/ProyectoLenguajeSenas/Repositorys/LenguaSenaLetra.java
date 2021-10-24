bbe161b892e4c8ba2e0ee8a55a648ff3c15c4016
Roger2608 <rogercabrera2608@gmail.com>
2021-10-24 02:16:22 -0500

    cambios en application

--- ProyectoLenguajeSenas/src/main/java/ProyectoLenguajeSenas/Repositorys/LenguaSenaLetra.java
+++
@@ -1,9 +0,0 @@
-package ProyectoLenguajeSenas.Repositorys;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ProyectoLenguajeSenas.Categorias.Letra;
-
-public interface LenguaSenaLetra extends JpaRepository<Letra, String> {
-
-}

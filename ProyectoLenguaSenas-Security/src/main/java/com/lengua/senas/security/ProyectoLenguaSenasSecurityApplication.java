5b14745d8d6235c8a6ffbf444fd9c86f86dd273d
Roger2608 <rogercabrera2608@gmail.com>
2021-11-06 00:28:56 -0500

    creando proyectos para aspectos y seguridad

---
+++ ProyectoLenguaSenas-Security/src/main/java/com/lengua/senas/security/ProyectoLenguaSenasSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.lengua.senas.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLenguaSenasSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLenguaSenasSecurityApplication.class, args);
+	}
+
+}
---
+++ ProyectoLenguaSenas-Security/src/test/java/com/lengua/senas/security/ProyectoLenguaSenasSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lengua.senas.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLenguaSenasSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
